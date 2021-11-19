2d28ca60c10c45feb140370ad0f446b48737a46e
phuihumu <priscilla.hui@sjsu.edu>
2021-11-18 17:44:17 -0800

    Created operate function that calls on the base functions

--- js-file.js
+++ js-file.js
@@ -15,3 +15,24 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1/num2;
 }
+
+function operate(operator, num1, num2)
+{
+    let result;
+    if (operator === "+")
+    {
+        result = add(num1,num2);
+    }
+    else if (operator === "-")
+    {
+        result = sub(num1,num2);
+    }
+    else if (operator === "*")
+    {
+        result = multiply(num1,num2);
+    }
+    else {
+        result = divide(num1,num2);
+    }
+    return result;
+}
