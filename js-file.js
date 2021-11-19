8fcda3d4d79666ea61e955396215f2545b95eee6
phuihumu <priscilla.hui@sjsu.edu>
2021-11-18 17:25:10 -0800

    Added base functions to the javascript file

---
+++ js-file.js
@@ -0,0 +1,17 @@
+/* Base calculator functions */
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function sub(num1, num2) {
+	return num1 - num2;
+};
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1/num2;
+}
